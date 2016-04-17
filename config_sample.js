1ed0706342897d94fa98a38f72231a99451fde15
Mike Wooldridge <mike@hellion.org>
2016-04-17 01:10:03 -0700

    Add files

---
+++ config_sample.js
@@ -0,0 +1,115 @@
+var config = {};
+
+config.path = "/PATH/TO/PROJECT/ml-bitemporal-example/"; // include trailing "/"
+
+config.host = "localhost";
+
+config.database = {
+  "name": "ml-bitemporal-example",
+  "port": 8554
+};
+
+config.auth = {
+  user: 'ML_USER',
+  pass: 'ML_PASSWORD',
+  sendImmediately: false
+};
+
+config.databaseSetup = {
+  "database-name": config.database.name,
+  "range-element-index": [
+    {
+      "collation": "",
+      "invalid-values": "reject",
+      "localname": "valStart",
+      "namespace-uri": "",
+      "range-value-positions": false,
+      "scalar-type": "dateTime"
+    },
+    {
+      "collation": "",
+      "invalid-values": "reject",
+      "localname": "valEnd",
+      "namespace-uri": "",
+      "range-value-positions": false,
+      "scalar-type": "dateTime"
+    },
+    {
+      "collation": "",
+      "invalid-values": "reject",
+      "localname": "sysStart",
+      "namespace-uri": "",
+      "range-value-positions": false,
+      "scalar-type": "dateTime"
+    },
+    {
+      "collation": "",
+      "invalid-values": "reject",
+      "localname": "sysEnd",
+      "namespace-uri": "",
+      "range-value-positions": false,
+      "scalar-type": "dateTime"
+    }
+  ]
+};
+
+config.forestSetup = {
+  "forest-name": config.database.name + '-1',
+  "database": config.database.name
+}
+
+config.restSetup = {
+  "rest-api": {
+    "name": config.database.name + "-rest",
+    "database": config.database.name,
+    "modules-database": config.database.name + "-modules",
+    "port": config.database.port,
+    "error-format": "json"
+  }
+}
+
+config.axisValidSetup = {
+  "axis-name": "axisValid",
+  "axis-start": {
+    "element-reference": {
+      "namespace-uri": "",
+      "localname": "valStart"
+    }
+  },
+  "axis-end": {
+    "element-reference": {
+      "namespace-uri": "",
+      "localname": "valEnd"
+    }
+  }
+};
+
+config.axisSystemSetup = {
+  "axis-name": "axisSystem",
+  "axis-start": {
+    "element-reference": {
+      "namespace-uri": "",
+      "localname": "sysStart"
+    }
+  },
+  "axis-end": {
+    "element-reference": {
+      "namespace-uri": "",
+      "localname": "sysEnd"
+    }
+  }
+};
+
+config.collectionSetup = {
+  "collection-name": "temporalCollection",
+  "system-axis": "axisSystem",
+  "valid-axis": "axisValid"
+};
+
+config.lsqtSetup = {
+  "lsqt-enabled": true
+};
+
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+  module.exports = config;
+}
---
+++ setup.js
@@ -0,0 +1,178 @@
+var config = require('./config'),
+    rp = require('request-promise'),
+    fs = require('fs');
+
+function handleError(err) {
+  if (err.error &&
+      err.error.errorResponse &&
+      err.error.errorResponse.message) {
+    console.log('Error: ' + err.error.errorResponse.message);
+  } else {
+    console.log(JSON.stringify(err, null, 2));
+  }
+}
+
+function createDatabase() {
+  var options = {
+    method: 'POST',
+    uri: 'http://' + config.host + ':8002/manage/v2/databases',
+    body: config.databaseSetup,
+    json: true,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    auth: config.auth
+  };
+  rp(options)
+    .then(function (parsedBody) {
+      console.log('Database created: ' + config.databaseSetup["database-name"]);
+      getHost();
+    })
+    .catch(function (err) {
+      handleError(err);
+    });
+}
+
+var hostName = '';
+
+function getHost() {
+  var options = {
+    method: 'GET',
+    uri: 'http://' + config.host + ':8002/manage/v2/hosts',
+    json: true,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    auth: config.auth
+  };
+  rp(options)
+    .then(function (parsedBody) {
+      hostName = parsedBody['host-default-list']['list-items']['list-item'][0].nameref;
+      console.log('Host name: ' + hostName);
+      createForest(hostName);
+    })
+    .catch(function (err) {
+      handleError(err);
+    });
+}
+
+function createForest(hostName) {
+  config.forestSetup["host"] = hostName;
+  var options = {
+    method: 'POST',
+    uri: 'http://' + config.host + ':8002/manage/v2/forests',
+    body: config.forestSetup,
+    json: true,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    auth: config.auth
+  };
+  rp(options)
+    .then(function (parsedBody) {
+      console.log('Forest created and attached: ' + config.forestSetup["forest-name"]);
+      createREST();
+    })
+    .catch(function (err) {
+      handleError(err);
+    });
+}
+
+function createREST() {
+  var options = {
+    method: 'POST',
+    uri: 'http://' + config.host + ':8002/v1/rest-apis',
+    body: config.restSetup,
+    json: true,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    auth: config.auth
+  };
+  rp(options)
+    .then(function (parsedBody) {
+      console.log('REST instance created at port: ' + config.restSetup["rest-api"]["port"]);
+      createValidAxis();
+    })
+    .catch(function (err) {
+      handleError(err);
+    });
+}
+
+function createAxis(axisConfig, callback) {
+  var options = {
+    method: 'POST',
+    uri: 'http://' + config.host + ':8002/manage/v2/databases/' + config.database.name + '/temporal/axes',
+    body: axisConfig,
+    json: true,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    auth: config.auth
+  };
+  rp(options)
+    .then(function (parsedBody) {
+      console.log('Temporal axis created: ' + axisConfig["axis-name"]);
+      if (callback) {
+        callback.call(this);
+      }
+    })
+    .catch(function (err) {
+      handleError(err);
+    });
+}
+
+function createValidAxis() {
+  createAxis(config.axisValidSetup, createSystemAxis);
+}
+
+function createSystemAxis() {
+  createAxis(config.axisSystemSetup, createCollection);
+}
+
+function createCollection() {
+  var options = {
+    method: 'POST',
+    uri: 'http://' + config.host + ':8002/manage/v2/databases/' + config.database.name + '/temporal/collections',
+    body: config.collectionSetup,
+    json: true,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    auth: config.auth
+  };
+  rp(options)
+    .then(function (parsedBody) {
+      console.log('Temporal collection created: ' + config.collectionSetup["collection-name"]);
+      lsqtSetup();
+    })
+    .catch(function (err) {
+      handleError(err);
+    });
+}
+
+function lsqtSetup() {
+  var options = {
+    method: 'PUT',
+    uri: 'http://' + config.host + ':8002/manage/v2/databases/' + config.database.name + '/temporal/collections/lsqt/properties?collection=temporalCollection',
+    body: config.lsqtSetup,
+    json: true,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    auth: config.auth
+  };
+  rp(options)
+    .then(function (parsedBody) {
+      console.log('LSQT enabled: ' + config.lsqtSetup["lsqt-enabled"]);
+    })
+    .catch(function (err) {
+      handleError(err);
+    });
+}
+
+function start() {
+  createDatabase();
+}
+
+start();
